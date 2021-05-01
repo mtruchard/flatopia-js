d0c2e1c0c4fd7a8a49512267bd1f9299d2ae4bd3
Alva Truchard <alvatruchard@gmail.com>
2021-05-01 16:28:35 -0500

    Work on Second Game

---
+++ games/secondGame/secondGame.js
@@ -0,0 +1,33 @@
+world = new World({
+    canvasId:"canvas",
+    consoleId:"console",
+    width:1000, height:800,
+});
+
+console.log("Hello World!");
+
+// Add a Bird to the world
+bird = new Character({
+    name:"Birdy", x:200, y:0, h:150,
+    imageUrl:"bird.png"
+});
+world.add(bird);
+
+// Add a Paper Doll to the world
+doll = new PaperDoll({x:-200, y:100, h:0});
+world.add(doll);
+
+/*
+world.addController( new DebugController() );
+world.addController( new CharacterController({
+    character:bird,
+    onEvent: function(e){}
+}));
+
+flower.moveTo({x:0,y:500,h:0});
+flower.moveTo({x:100,y:500,h:0});
+flower.moveTo({x:-100,y:500,h:0});
+flower.moveTo({x:-100,y:500,h:100});
+flower.moveTo({x:50,y:500,h:100});
+flower.moveTo({x:0,y:0,h:0});
+*/
